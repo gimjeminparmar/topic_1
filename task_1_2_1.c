07e4f98847e58842abd498a7747e5098eb2c3200
gimjeminparmar <jeminparmar115@gmail.com>
2024-06-20 12:16:59 +0530

    Add files via upload

---
+++ task_1_2_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main()
+{
+	printf("name : jemin\n");
+	printf("surename : parmar\n");
+	printf("father's name : hiteshbhai\n");
+	printf("age : 18\n");
+	printf("school name : paramsukh gurukul");
+}
