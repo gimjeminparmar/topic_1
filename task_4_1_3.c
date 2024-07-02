a16ea2d8b8e8ce67411936f2618c43d761262084
gimjeminparmar <jeminparmar115@gmail.com>
2024-07-02 12:43:51 +0530

    Add files via upload

---
+++ task_4_1_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	int x,y,xy;
+	printf("enter value x =");
+	scanf("%d",&x);
+	printf("enter value y =");
+	scanf("%d",&y);
+	 xy = (x*x*x) + (3*(x*x*y)) + (3*(x*y*y)) + (y*y*y);
+	 
+	 printf("formula answer =%d",xy);
+}
